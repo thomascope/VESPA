0f2af86e1d4c81680a958e12e0388f1ebf4beb19
thomascope <thomascope@gmail.com>
2016-10-12 14:15:52 +0100

    New function for committing files to git

--- SPM12version/Standalone preprocessing pipeline/git_commit.m
+++ SPM12version/Standalone preprocessing pipeline/git_commit.m
@@ -2,4 +2,5 @@ function git_commit(filename, message)
 
 !git config --global user.name "thomascope"
 !git config --global user.email 'thomascope@gmail.com'
+eval(['!git add ''./' filename])
 eval(['!git commit ''./' filename ''' -m ''' message ''''])
